c153ab94e76e6535d8f40aae179e42b478f1c07e
Otavio Perkles <otaviocha@gmail.com>
2018-01-23 09:14:44 -0200

    Saiu ?

--- portfolio/about_me/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.contrib import admin
-from django.conf.urls import url
-
-urlpatterns = [
-    url(r'^', views.sobremim_page),
-]
--- portfolio/portfolio_page/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PortfolioPageConfig(AppConfig):
-    name = 'portfolio_page'
